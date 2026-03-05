79a07e74ff5de4af3010c403eb0f624b00246535
soni168 <chaudharysoni149@gmail.com>
2026-03-05 12:27:01 +0530

    added changes

--- server/config/email.js
+++ server/config/email.js
@@ -2,9 +2,9 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  family: 4, // IPv4 force karo
+  port: 465,
+  secure: true,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
